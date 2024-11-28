ba80449210592aa7e361d02066b683fe31f88792
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2024-11-28 12:23:01 +0100

    test: Fix parallel test in simd mode

--- src/sampler.rs
+++ src/sampler.rs
@@ -979,7 +979,7 @@ pub mod test_logps {
                 for (p, g) in pos.chunks_exact(4).zip(grad.chunks_exact_mut(4)) {
                     let p = f64x4::from_slice(p);
                     let val = mu_splat - p;
-                    logp = val * val * f64x4::splat(0.5);
+                    logp -= val * val * f64x4::splat(0.5);
                     g.copy_from_slice(&val.to_array());
                 }
 
